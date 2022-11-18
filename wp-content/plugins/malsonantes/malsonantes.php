1b8308acb3abc842c8df1f7e3a8f61a8dd7fd277
martaperezperez <mperezperez@danielcastelao.org>
2022-11-18 12:28:31 +0100

    tabla de base de datos

--- wp-content/plugins/malsonantes/malsonantes.php
+++ wp-content/plugins/malsonantes/malsonantes.php
@@ -24,8 +24,26 @@ function cambiar_malsonantes($text)
  */
 add_filter('the_content', 'cambiar-malsonantes');
 
-function jal_install(){
+
+function Crearbasededatos(){
     global $wpdb;
 
-    $table_name = $wpdb ->prefix . "liveshoutbox";
+    $charset_collate = $wpdb->get_charset_collate();
+
+// le añado el prefijo a la tabla
+    $table_name = $wpdb->prefix . 'dam';
+
+// creamos la sentencia sql
+
+    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+id mediumint(9) NOT NULL AUTO_INCREMENT,
+time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+name tinytext NOT NULL,
+text text NOT NULL,
+url varchar(55) DEFAULT '' NOT NULL,
+PRIMARY KEY (id)
+) $charset_collate;";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
 }
